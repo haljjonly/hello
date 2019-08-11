cf5eab09a585c03ae9a2e04e95f3f01db5ada88a
洪桢 <hongzhen@hongzhendeMacBook-Pro.local>
2019-08-11 22:27:04 +0800

    输出456

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args)
     {
-        System.out.println(123);
+        System.out.println(45666);
         SpringApplication.run(DemoApplication.class, args);
     }
 
